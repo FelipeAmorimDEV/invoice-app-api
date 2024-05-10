3c3632cf012e4b3fc3945a2216f8cd92fd284390
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-05-10 14:33:03 -0300

    feat: deleteInvoice,makeInvoicedPaid and editInvoice method to UserRepository

--- src/repository/in-memory-repository/in-memory-invoice-repository.ts
+++ src/repository/in-memory-repository/in-memory-invoice-repository.ts
@@ -2,7 +2,45 @@ import { Invoice, Prisma } from "@prisma/client";
 import { InvoiceRepository } from "@/repository/invoice-repository";
 
 export class InMemoryInvoiceRepository implements InvoiceRepository {
+
   public invoices: Invoice[] = []
+
+  async editInvoice(id: number, {clientAddress, clientEmail,clientName,description,dueAt,items,senderAddress,status,terms,total}: Prisma.InvoiceUncheckedCreateInput) {
+    const invoice = this.invoices.find(invoice => invoice.id === id)
+
+    if (!invoice) {
+      return null
+    }
+
+    const invoiceEdited = {...invoice, clientAddress: JSON.stringify(clientAddress), clientEmail,clientName,description,dueAt: new Date(dueAt),items: JSON.stringify(items),senderAddress: JSON.stringify(senderAddress),status,terms,total}
+    
+    return invoiceEdited
+  }
+
+  async markInvoiceAsPaid(id: number) {
+    const invoice = this.invoices.find(invoice => invoice.id === id)
+
+    if (!invoice) {
+      return null
+    }
+
+    invoice.status = 'paid'
+
+    return invoice
+  }
+
+  async deleteInvoice(id: number) {
+    const invoiceIndex = this.invoices.findIndex(invoice => invoice.id === id)
+
+    if(invoiceIndex < 0) {
+      return null
+    }
+
+    const withoutItem = this.invoices.splice(invoiceIndex, 1)
+    
+    return withoutItem
+  }
+
   async createInvoice(data: Prisma.InvoiceUncheckedCreateInput) {
     const invoice = {
       id: 1,
--- src/repository/invoice-repository.ts
+++ src/repository/invoice-repository.ts
@@ -2,4 +2,7 @@ import { Prisma, Invoice } from "@prisma/client";
 
 export interface InvoiceRepository {
   createInvoice(data: Prisma.InvoiceUncheckedCreateInput): Promise<Invoice>
+  deleteInvoice(id: number): Promise<Invoice[] | null>
+  markInvoiceAsPaid(id: number): Promise<Invoice | null>
+  editInvoice(id: number, data: Prisma.InvoiceUncheckedCreateInput): Promise<Invoice | null>
 }
