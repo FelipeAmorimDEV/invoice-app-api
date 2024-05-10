8f348cab04068390c573d7256070ff3f9dd3d2b5
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-05-09 23:18:45 -0300

    feat: add createInvoiceUseCase factorie

---
+++ src/usecases/factories/make-create-invoice-user-case.ts
@@ -0,0 +1,11 @@
+import { PrismaInvoiceRepository } from "../../repository/prisma/prisma-invoice-repository";
+import { PrismaUserRepository } from "../../repository/prisma/prisma-user-repository";
+import { CreateInvoiceUseCase } from "../create-invoice-usecase";
+
+export function makeCreateInvoiceUseCase() {
+  const userRepository = new PrismaUserRepository()
+  const invoiceRepository = new PrismaInvoiceRepository()
+  const createInvoiceUserCase = new CreateInvoiceUseCase(invoiceRepository, userRepository)
+
+  return createInvoiceUserCase
+}
