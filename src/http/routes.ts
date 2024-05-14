ebf7888ee8ff341c61f2e56e5cabc77c1668f9d0
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-05-14 11:29:40 -0300

    feat: create user and some invoice routers

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,19 @@
 import { FastifyInstance} from 'fastify'
-import { createUser } from './controllers/create-user'
-import { createInvoice } from './controllers/create-invoice'
+import { createUser } from './controllers/user/create-user'
+import { authenticateUser } from './controllers/user/authenticate-user'
+import { createInvoice } from './controllers/invoices/create-invoice'
+import { fetchInvoices } from './controllers/invoices/fetch-invoices'
+import { getInvoice } from './controllers/invoices/get-invoice'
+import { deleteInvoice } from './controllers/invoices/delete-invoice'
+import { markStatusAsPaid } from './controllers/invoices/paid-status'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', createUser)
+  app.post('/users/login', authenticateUser)
+
   app.post('/invoices', createInvoice)
+  app.get('/invoices/:id', getInvoice)
+  app.get('/invoices', fetchInvoices)
+  app.delete('/invoices/:id', deleteInvoice)
+  app.patch('/invoices/:id', markStatusAsPaid)
 }
