18d4222e0d55c7cad62265c79a549ef29dc8e37a
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-05-14 12:35:01 -0300

    feat: calculate total and dueAt

--- src/usecases/edit-invoice-use-case.ts
+++ src/usecases/edit-invoice-use-case.ts
@@ -1,36 +1,33 @@
 import { Invoice } from "@prisma/client";
 import { InvoiceRepository } from "@/repository/invoice-repository";
-import { UserRepository } from "@/repository/user-repository";
 import { ResouceNotFoundError } from "./errors/resource-not-found-error";
+import { addDays } from "date-fns";
 
 interface EditInvoiceUseCaseParams {
   id: number
-  userId: string
-  description: string
-  dueAt: string
-  status: string
-  terms: string
-  clientName: string
-  clientEmail: string
-  senderAddress: {
+  description?: string 
+  terms: number
+  createdAt: string | Date
+  clientName?: string
+  clientEmail?: string 
+  senderAddress?: {
     street: string
     city: string
     postCode: string
     country: string
   }
-  clientAddress: {
+  clientAddress?: {
     street: string
     city: string
     postCode: string
     country: string
-  }
-  items: {
+  } 
+  items?: {
     name: string
     quantity: number
     price: number
     total: number
   }[]
-  total: number
 }
 
 interface EditInvoiceUseCaseResponse {
@@ -40,8 +37,14 @@ interface EditInvoiceUseCaseResponse {
 export class EditInvoiceUseCase {
   constructor(private invoiceRepository: InvoiceRepository) { }
 
-  async execute({ description, clientName, clientEmail, clientAddress, senderAddress, status, terms, dueAt, items, total, id, userId}: EditInvoiceUseCaseParams): Promise<EditInvoiceUseCaseResponse> {
-    const invoice = await this.invoiceRepository.editInvoice(id, { description, clientName, clientEmail, clientAddress, senderAddress, status, terms, dueAt, items, total, userId })
+  async execute({ description, clientName, clientEmail, clientAddress, senderAddress,terms, items, id, createdAt}: EditInvoiceUseCaseParams): Promise<EditInvoiceUseCaseResponse> {
+
+    const dueAt = addDays(new Date(createdAt), terms)
+    const total = items ? items.reduce((acc,item) => acc+= item.total, 0) : 0
+
+    const date = { description, clientName, clientEmail, clientAddress, senderAddress, terms, dueAt, items, total }
+
+    const invoice = await this.invoiceRepository.editInvoice(id, date)
 
     if (!invoice) {
       throw new ResouceNotFoundError()
