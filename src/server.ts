d9b96d68c12dd6e10de942be89d0f95252a5e22a
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-06-10 13:32:59 -0300

    feat: add port enviroment

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
-    port: 3333,
+    host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running.')
